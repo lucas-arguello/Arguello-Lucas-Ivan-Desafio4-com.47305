4a3f2886915831fdaebdcfba08a05ff707517988
Lucas Arguello <lucas5ivan@hotmail.com>
2023-09-19 09:40:38 -0300

    modif. realtime

--- e-commerce/src/public/js/realtime.js
+++ e-commerce/src/public/js/realtime.js
@@ -5,17 +5,32 @@ const socketClient = io();
 
 //Obtenemos los elementos a manipular en el HTML por medio de su "id".
 const productList = document.getElementById("productList")
-const createProductFrom = document.getElementById("createProductFrom")
+const createProductForm = document.getElementById("createProductForm")
 
 //1ro. a los elementos del form, le agregamos el evento "submit",
-//y capturamos la info del form. en una funcion
-createProductFrom.addEventListener("submit", (e) => {
+//y capturamos la info del form. en una funcion, para poder enviar la info al servidor.
+createProductForm.addEventListener("submit",(e)=>{
     
     e.preventDefault();
-    console.log(e);
-
+    //console.log(e);
+    //en esta variable capturamos todos los campos del formulario.
+    const formData = new FormData(createProductForm);
+    // console.log(formData.get("title"))
+    //creamos el objeto JSON que vamos enviar con la Data, e iteramos con un (for- of) un arreglo, para crear un objeto
+    const jsonData = {};
+    for(const [key,value] of formData.entries()){
+        jsonData[key]=value
+    }; 
+    jsonData.stock = parseInt(jsonData.stock);
+    jsonData.price = parseInt(jsonData.price);
+    console.log(jsonData);
+    //ahora enviamos la info del formulario (jsonData) al socket del servidor
+    socketClient.emit("addProduct", jsonData);
+    //con el metodo "reset" hacemos vacios los campos una vez enviado el producto.
+    createProductForm.reset();
 });
-//ahora enviamos la info del formulario al socket del servidor
+
+
 
 
 //recibimos los productos desde el server de websocket.
